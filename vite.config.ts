4a884fc5be088d63e10a39d84901a48529beb25c
todor534 <t.todorov534@gmail.com>
2025-10-26 16:21:41 +0200

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,3 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({ plugins: [react()], base: "/", build: { sourcemap: true } });
