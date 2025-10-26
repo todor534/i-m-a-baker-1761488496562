0448f75455102157a8899c977a9eaf46ca8046a1
todor534 <t.todorov534@gmail.com>
2025-10-26 16:21:40 +0200

    chore: update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
